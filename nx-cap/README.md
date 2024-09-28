064491f12306de6a0175fe091f7c74ef341a327f
Petr Plenkov <petr.plenkov@gmail.com>
2024-09-28 20:15:41 +0200

    nx-cap plugin

---
+++ nx-cap/README.md
@@ -0,0 +1,11 @@
+# nx-cap
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-cap` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-cap` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ nx-cap/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'nx-cap',
+  preset: '../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/nx-cap',
+};
