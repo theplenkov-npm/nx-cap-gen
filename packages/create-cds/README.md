e1f93626b3f4d67b2dc80104032254ca4d59978a
Petr Plenkov <petr.plenkov@gmail.com>
2024-09-30 17:07:06 +0200

    create-cds

---
+++ packages/create-cds/README.md
@@ -0,0 +1,11 @@
+# create-cds
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build create-cds` to build the library.
+
+## Running unit tests
+
+Run `nx test create-cds` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/create-cds/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'create-cds',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/create-cds',
+};
